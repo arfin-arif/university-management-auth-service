c5fdde55d6df50767c2ee1f6c00de4583b89534f
arfin-arif <arifmtbd@gmail.com>
2023-06-02 20:12:07 +0600

    [DONE] setup express,mongoose,cors,parser

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "../config/index";
+
+async function bootstrap() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log(`\nDatabase connected `);
+
+        app.listen(config.port, () => {
+            console.log(`listening on port ${config.port}`)
+        })
+    } catch (error) {
+        console.log(`Failed to connected the database`);
+    }
+}
+bootstrap()
