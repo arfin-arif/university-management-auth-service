06a2fe4582934f1d5aa3f442ba9d157fc4474b57
arfin-arif <arifmtbd@gmail.com>
2023-06-13 01:33:56 +0600

    [DONE update validation]

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -4,6 +4,8 @@ import {
   IAcademicSemester,
 } from './academicSemester.interface';
 import { academicSemesterMonths } from './academicSemester.constant';
+import ApiError from '../../../errors/ApiErrors';
+import status from 'http-status';
 
 const academicSemesterSchema = new Schema<IAcademicSemester>(
   {
@@ -17,6 +19,21 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
     timestamps: true,
   }
 );
+
+academicSemesterSchema.pre('save', async function (next) {
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  });
+
+  if (isExist) {
+    throw new ApiError(
+      status.CONFLICT,
+      'Academic Semester is already is exist'
+    );
+  }
+  next();
+});
 
 export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
   'AcademicSemester',
